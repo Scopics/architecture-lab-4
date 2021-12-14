7c205b771fd80f8904bac7d93fb10c86a9c44be7
YAGoOaR <yegorgribenko68@gmail.com>
2021-12-14 19:00:56 +0200

    Add more commands

---
+++ commands/random.go
@@ -0,0 +1,18 @@
+package commands
+
+import (
+	"math/rand"
+	"strconv"
+
+	"github.com/Scopics/architecture-lab-4/engine"
+)
+
+type random struct {
+	arg1, arg2 int
+}
+
+func (p *random) Execute(loop engine.Handler) {
+	r := rand.New(rand.NewSource(99))
+	res := r.Int()
+	loop.Post(&print{Arg: strconv.Itoa(res)})
+}
---
+++ commands/subtract.go
@@ -0,0 +1,16 @@
+package commands
+
+import (
+	"strconv"
+
+	"github.com/Scopics/architecture-lab-4/engine"
+)
+
+type subtract struct {
+	arg1, arg2 int
+}
+
+func (p *subtract) Execute(loop engine.Handler) {
+	res := p.arg1 - p.arg2
+	loop.Post(&print{Arg: strconv.Itoa(res)})
+}
