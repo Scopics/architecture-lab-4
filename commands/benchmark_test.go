ce7599129fbfc4ac64226034df1e8c7b7eaac6de
MaksGovor <maksgovruha@gmail.com>
2021-12-20 21:54:58 +0200

    Add variable to test

--- commands/benchmark_test.go
+++ commands/benchmark_test.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+
+	"github.com/Scopics/architecture-lab-4/engine"
 )
+
+var cmd engine.Command
 
 func BenchmarkParse(b *testing.B) {
 	for i := 1; i <= 20; i++ {
 		b.Run(fmt.Sprintf("%d-length", i*1000), func(b *testing.B) {
-			Parse("palindrom " + strings.Repeat("A", i*10000))
+			cmd = Parse("palindrom " + strings.Repeat("A", i*10000))
 		})
 	}
 }
